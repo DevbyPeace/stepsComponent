66a494640f541972d7476fa6b681498ff822bc6f
Peace <devbypeace@gmail.com>
2025-07-11 21:09:23 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default function App() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  /// Eventhandler Functions for Prev and Next buttons
+  function handlePrevious() {
+    if (step > 1) setStep((s) => s - 1);
+  }
+  function handleNext() {
+    if (step < 3) setStep((s) => s + 1);
+  }
+
+  return (
+    <>
+      <button className="close" onClick={() => setIsOpen((open) => !open)}>
+        {isOpen ? <>&times;</> : <>&#9633;</>}
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
+
+          <div className="buttons">
+            <Button bgColor="#7950F2" textColor="#fff" onClick={handlePrevious}>
+              👈🏻 Previous
+            </Button>
+            <Button
+              bgColor="#7950F2"
+              textColor="#fff"
+              onClick={handleNext}
+              text="Next"
+              emoji="👉🏻"
+            >
+              Next 👉🏻
+            </Button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3> step {step}</h3>
+      {children}
+    </div>
+  );
+}
+
+function Button({ textColor, bgColor, onClick, children }) {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: textColor }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
